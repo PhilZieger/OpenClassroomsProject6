7f43b68e25683699ba58edd08e924c29e1acb968
Phil Zieger <phil.zieger.pro@gmail.com>
2023-04-22 11:53:13 +0200

    Header Style Modified

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,25 @@
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import Logo from '../../assets/logo_red.png'
 import '../../styles/header.css'
 
 function Header() {
+
+
     return (
       <nav className='kasa-header'>
         <Link to="/">
           <img alt="Logo Kasa" src={Logo} />
         </Link>
         <div className='kasa-nav'>
-          <Link to="/">Accueil</Link>
-          <Link to="/about">A Propos</Link>
+          <NavLink 
+            to="/" 
+            className={({ isActive }) => (isActive ? 'selected' : null)
+            }>Accueil</NavLink>
+
+          <NavLink 
+          to="/about" 
+          className={({ isActive }) => (isActive ? 'selected' : null)
+          }>A Propos</NavLink>
         </div>
       </nav>
     )
